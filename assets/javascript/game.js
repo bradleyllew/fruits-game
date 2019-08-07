1913e89dd332e102598da84c74ed7f54841b211f
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-08-07 18:20:24 -0400

    took extra ids out of images and set vars for each fruit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,113 @@
 console.log("Am I working?");
 
-var winsSelector = document.getElementById('wins');
-var lossSelector = document.getElementById('losses');
+// var winsSelector = document.getElementById('wins');
+// var lossSelector = document.getElementById('losses');
 
 
-var counter = 0;
-var randomNumber;
-var wins;
-var losses;
+var score = 0;
+// var randomNumber;
+var wins = 0;
+var losses = 0;
 var targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-
 console.log(targetNumber);
 
-$("#numberToGuess").text(targetNumber);
-// $("#number-to-guess").text(targetNumber);
 
-$(".img-responsive").on("click", function() {
-    counter += 1;
-    console.log(counter);
-  });
+// var reset = function () {
 
-var fruits = ["apple", "banana", "berry", "melon",]
+    $("#numberToGuess").text(targetNumber);
 
-for(var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 12);
-    console.log(random);
-    var fruits = $("<div>");
-    fruits.addClass("fruits");
-    fruits.attr({
-    "data-fruit-value":  random
-});
-    $(".fruits").append(fruits);
-    console.log(fruits);
-}
+    var fruitOptions = ["apple", "banana", "berry", "melon"]
+    
+    for (var i = 0; i < 4; i++) {
 
-$(".fruits").on("click", function() {
-    var fruitValue = ($(this).attr("data-fruit-value"));
+        var random = Math.floor(Math.random() * 11) + 1;
+        console.log(random);
+
+        var apple = $("#apple");
+        apple.addClass("fruitsRow");
+        apple.attr("data-fruit-val", random);
+        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
+        $(".fruitsRow").append(apple);
+        
+        var banana = $("#banana");
+        banana.addClass("fruitsRow");
+        banana.attr("data-fruit-val", random);
+        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
+        $(".fruitsRow").append(banana);
+
+        var berry = $("#berry");
+        berry.addClass("fruitsRow");
+        berry.attr("data-fruit-val", random);
+        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
+        $(".fruitsRow").append(berry);
+
+        var melon = $("#melon");
+        melon.addClass("fruitsRow");
+        melon.attr("data-fruit-val", random);
+        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
+        $(".fruitsRow").append(melon);
+        
+        
+        
+        
+        
+        
+        // var imageFruit = $("<div>");
+
+        // imageFruit.addClass("fruit-image");
+
+        // imageFruit.attr("src", "assets/images/apple.jpg");
+        // // imageFruit.attr("data-fruit-val", random);
+        // imageFruit.attr({ "#apple": 'fruit', "data-random": random});
+        // $(".fruit-image").append(imageFruit);
+        
+        // var appleBtn = $("<img>");
+        // appleBtn.addClass("imageFruit");
+        // appleBtn.attr("src", "/assets/images/apple.jpg");
+        // appleBtn.attr("data-random");
+        
+
+        // var random = Math.floor(Math.random() * 11) + 1;
+        // console.log(random);
+
+        // var fruits = $("<div>");
+        // fruits.addClass("fruits-row");
+        // fruits.attr({
+        //     "data-fruit-value": random
+        // });
+        // $("fruits-row").append(fruits);
+        // console.log(fruits);
+    }
+// }
+
+$(".fruits").on('click', function () {
+    // console.log($('.fruits'));
+    var fruitValue = ($(this).attr('data-fruit-val'));
     fruitValue = parseInt(fruitValue);
-    counter += fruitValue;
+    score += fruitValue;
+    console.log(fruitValue);
 
-    if (counter === targetNumber) {
-    wins++;
-    winsSelector.innerText = +1;
-}
 
-if (counter > targetNumber) {
-    losses++;
-    lossSelector.innerText = +1;
-}
+    if (score === targetNumber) {
+        console.log("win");
+        wins++;
+        $("#wins").html(wins);
+        // reset();
+    }
+
+    if (score > targetNumber) {
+        console.log("fail");
+        losses++;
+        $("losses").html(losses);
+        // reset();
+    }
 
 });
+
+// $(".img-responsive").on("click", function () {
+//     // score += 1;
+//     console.log(score);
+//     $("score").html(score);
 
 
 
