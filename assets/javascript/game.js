fb198f9dc8a724fa1567dd15acee98c52f502eac
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-08-07 12:40:41 -0400

    Got target number to generate as random b/t 19-120 and to appear in correct placement. Still need wins/losses to appear and to append fruit clicks towards total

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,55 @@
 console.log("Am I working?");
 
+var winsSelector = document.getElementById('wins');
+var lossSelector = document.getElementById('losses');
+
+
 var counter = 0;
 var randomNumber;
 var wins;
 var losses;
-var targetNumber;
+var targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
+console.log(targetNumber);
 
-$("#number-to-guess").text(targetNumber);
+$("#numberToGuess").text(targetNumber);
+// $("#number-to-guess").text(targetNumber);
 
 $(".img-responsive").on("click", function() {
     counter += 1;
     console.log(counter);
   });
 
-
+var fruits = ["apple", "banana", "berry", "melon",]
 
 for(var i = 0; i < 4; i++) {
     var random = Math.floor(Math.random() * 12);
     console.log(random);
+    var fruits = $("<div>");
+    fruits.addClass("fruits");
+    fruits.attr({
+    "data-fruit-value":  random
+});
+    $(".fruits").append(fruits);
+    console.log(fruits);
 }
 
-if (counter === targetNumber) {
-    wins++
+$(".fruits").on("click", function() {
+    var fruitValue = ($(this).attr("data-fruit-value"));
+    fruitValue = parseInt(fruitValue);
+    counter += fruitValue;
+
+    if (counter === targetNumber) {
+    wins++;
+    winsSelector.innerText = +1;
 }
 
 if (counter > targetNumber) {
-    losses++
+    losses++;
+    lossSelector.innerText = +1;
 }
+
+});
 
 
 
