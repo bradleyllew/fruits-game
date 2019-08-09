a6ce941754910e2f57e636ac770fcefcb82b1b40
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-08-08 21:34:59 -0400

    trying to get target number to refresh after win/loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,113 +1,57 @@
 console.log("Am I working?");
 
-// var winsSelector = document.getElementById('wins');
-// var lossSelector = document.getElementById('losses');
 
 
 var score = 0;
-// var randomNumber;
 var wins = 0;
 var losses = 0;
 var targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 console.log(targetNumber);
 
+$("#numberToGuess").text(targetNumber);
 
-// var reset = function () {
+var fruitOptions = ["apple", "banana", "berry", "melon"]
 
-    $("#numberToGuess").text(targetNumber);
+for (var i = 0; i < 4; i++) {
 
-    var fruitOptions = ["apple", "banana", "berry", "melon"]
-    
-    for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 11) + 1;
+    console.log(random);
 
-        var random = Math.floor(Math.random() * 11) + 1;
-        console.log(random);
-
-        var apple = $("#apple");
-        apple.addClass("fruitsRow");
-        apple.attr("data-fruit-val", random);
-        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
-        $(".fruitsRow").append(apple);
-        
-        var banana = $("#banana");
-        banana.addClass("fruitsRow");
-        banana.attr("data-fruit-val", random);
-        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
-        $(".fruitsRow").append(banana);
-
-        var berry = $("#berry");
-        berry.addClass("fruitsRow");
-        berry.attr("data-fruit-val", random);
-        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
-        $(".fruitsRow").append(berry);
-
-        var melon = $("#melon");
-        melon.addClass("fruitsRow");
-        melon.attr("data-fruit-val", random);
-        // apple.attr({"fruitsRow": 'data-fruit-val', "data-random": random});
-        $(".fruitsRow").append(melon);
-        
-        
-        
-        
-        
-        
-        // var imageFruit = $("<div>");
-
-        // imageFruit.addClass("fruit-image");
-
-        // imageFruit.attr("src", "assets/images/apple.jpg");
-        // // imageFruit.attr("data-fruit-val", random);
-        // imageFruit.attr({ "#apple": 'fruit', "data-random": random});
-        // $(".fruit-image").append(imageFruit);
-        
-        // var appleBtn = $("<img>");
-        // appleBtn.addClass("imageFruit");
-        // appleBtn.attr("src", "/assets/images/apple.jpg");
-        // appleBtn.attr("data-random");
-        
-
-        // var random = Math.floor(Math.random() * 11) + 1;
-        // console.log(random);
-
-        // var fruits = $("<div>");
-        // fruits.addClass("fruits-row");
-        // fruits.attr({
-        //     "data-fruit-value": random
-        // });
-        // $("fruits-row").append(fruits);
-        // console.log(fruits);
-    }
-// }
+    var fruit = $("#" + fruitOptions[i]);
+    fruit.addClass("fruitsRow");
+    fruit.attr("data-fruit-val", random);
+    $(".fruitsRow").append(fruit);
+}
 
 $(".fruits").on('click', function () {
     // console.log($('.fruits'));
     var fruitValue = ($(this).attr('data-fruit-val'));
     fruitValue = parseInt(fruitValue);
     score += fruitValue;
+    $("#score").html(score);
     console.log(fruitValue);
+
+    if (score > targetNumber) {
+        console.log("fail");
+        losses++;
+        $("#losses").html(losses);
+        $("#score").html(score = 0);
+
+    }
 
 
     if (score === targetNumber) {
         console.log("win");
         wins++;
         $("#wins").html(wins);
-        // reset();
+        $("#score").html(score = 0);
+
     }
 
-    if (score > targetNumber) {
-        console.log("fail");
-        losses++;
-        $("losses").html(losses);
-        // reset();
-    }
+
 
 });
 
-// $(".img-responsive").on("click", function () {
-//     // score += 1;
-//     console.log(score);
-//     $("score").html(score);
 
 
 
@@ -139,4 +83,9 @@ $(".fruits").on('click', function () {
 
 
 
+
+
+function newFunction() {
+    $(targetNumber).html("#numberToGuess");
+}
 
